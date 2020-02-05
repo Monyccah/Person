0f46246dd1cd9d23be2e6ce259c2d17a38529f60
Monyccah <moniccahmadingwane@gmail.com>
2020-02-05 14:11:09 +0200

    initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    String name;
+     int age;
+     String gender;
+     String[] interest;
+
+
+
+    public Person(String name, int age, String gender, String[] interest) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.interest = interest;
+    }
+
+    public String hello() {
+        String message = "Hello, my name is "+ name + " and I am "+ age + " years old. My interests are " + interest[0] + ", " + interest[1] + " and " + interest[2] +".";
+        return message;
+    }
+}
